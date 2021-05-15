51ea52ef308f617b4b09f4212f8ad83d529c5b7e
Monika <monika.helfer@outlook.com>
2021-05-15 08:34:34 +0200

    Fixed the notifications error

--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -8,9 +8,11 @@ function Notification(props) {
             <h2>Dzisiaj oczekują podlania:</h2>
                 <ul>
                     {props.plantList.map((plant, index) => {
+                        return(
                             <div key={index} className="plant-list_list">
                                 <li>{plant.species}</li>
                             </div>
+                        )
                     })}
                 </ul>
             </div>
